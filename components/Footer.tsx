2f7b8deb523b11a6c46b9f8ec8947e07e471d063
Sujan Maji <dev.sujanmaji@gmail.com>
2023-09-27 19:37:46 +0530

    update copy right in footer area

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-white border-2">
       <div className="mx-auto py-10">
         <p className="text-center text-xs text-black">
-            &copy; 2023 FakeStoreName, Inc. All rights reseved.
+          &copy; 2023 SM-Store, Inc. All rights reseved.
         </p>
       </div>
     </footer>
